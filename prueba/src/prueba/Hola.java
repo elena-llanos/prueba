57276239d4a127aaabb5675d94b4e348abe371a1
in1daw <in1daw@HZ352011.elorrieta.int>
2022-03-09 15:22:30 +0100

    New hello world print

--- prueba/src/prueba/Hola.java
+++ prueba/src/prueba/Hola.java
@@ -1,5 +1,13 @@
 package prueba;
 
 public class Hola {
-
+	
+	private static int x = 10;
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hola mundo");
+		System.out.println("X = " + x);
+	}
 }
